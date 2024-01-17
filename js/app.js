af4e8576275fd383e48a9a67a46ca22a6d3f44c6
peklef <107624373+peklef@users.noreply.github.com>
2024-01-17 19:20:54 +0300

    first commit

---
+++ js/app.js
@@ -0,0 +1,4 @@
+var titleText = 'Привет, мой друг! Добро пожаловать на сайт группы отелей Selly Hotels!';
+
+var promoTitle = document.getElementById('promoTitle'); // получаем заголовок страницы
+promoTitle.innerText = titleText; // заменяем текст в заголовке
